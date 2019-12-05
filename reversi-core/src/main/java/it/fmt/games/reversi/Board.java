1d8f64a9ff4c2a9e1e99df0d062d43f593fcf8b0
Matteo <teo_exc@hotmail.it>
2019-12-05 09:48:16 +0100

    fix #3 reversi-#3 Entity: board

--- reversi-core/src/main/java/it/fmt/games/reversi/Board.java
+++ reversi-core/src/main/java/it/fmt/games/reversi/Board.java
@@ -1,17 +1,25 @@
 package it.fmt.games.reversi;
 
 public class Board {
-    private static final int BOARD_SIZE=8;
+    private static final int BOARD_SIZE = 8;
     private Cell[][] board;
 
+    public void setCell(Coordinates c, Piece p) {
+        board[c.getRow()][c.getColumn()] = new Cell(c, p);
+    }
+    public Cell getCell(Coordinates c){
+        return board[c.getRow()][c.getColumn()];
+    }
+
     public Board() {
-        board=new Cell[BOARD_SIZE][];
-        for(int i=0; i< BOARD_SIZE;i++) {
-            board[i]=new Cell[BOARD_SIZE];
+        board = new Cell[BOARD_SIZE][];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            board[i] = new Cell[BOARD_SIZE];
             for (int j = 0; j < BOARD_SIZE; j++)
-                board[i][j] = new Cell(new Coordinates(i,j),Piece.EMPTY);
+                board[i][j] = new Cell(new Coordinates(i, j), Piece.EMPTY);
         }
     }
+
 }
 
 
--- reversi-core/src/test/java/it/fmt/games/reversi/BoardTest.java
+++ reversi-core/src/test/java/it/fmt/games/reversi/BoardTest.java
@@ -2,11 +2,23 @@ package it.fmt.games.reversi;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
     @Test
-    public void newBoard() { Board b = new Board();  }
+    public void newBoard() {
+        Board b = new Board();
+    }
+
+    @Test
+    public void setCell() {
+        Coordinates coords1 = Coordinates.of("A1");
+        Board b =new Board();
+        b.setCell(coords1,Piece.PLAYER_1);
+        assertThat(b.getCell(coords1).getPiece(), equalTo(Piece.PLAYER_1));
+    }
 
 
 }
