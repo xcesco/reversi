f6a9f0b62bd65bf0e5ffd2a8bcf9068401466e07
Matteo <teo_exc@hotmail.it>
2019-12-06 12:14:40 +0100

    reversi-#16 Improve test coverage on Coordinates.isValid

--- reversi-core/src/test/java/it/fmt/games/reversi/CoordinatesTest.java
+++ reversi-core/src/test/java/it/fmt/games/reversi/CoordinatesTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CoordinatesTest {
 
