e3042ab43b3674a331f7d67bc4f0105fd58a2198
taz00 <tbonifazi91@gmail.com>
2019-12-08 16:01:18 +0100

    fix #19 reversi-#19 Improve test coverage and remove smells on class AvailableMoves (covered test on method equals)

--- reversi-core/src/test/java/it/fmt/games/reversi/CellTest.java
+++ reversi-core/src/test/java/it/fmt/games/reversi/CellTest.java
@@ -23,6 +23,8 @@ public class CellTest {
         assertThat(cell, not(cellNotEqual));
         assertThat(cell, not(cellNotEqual2));
         assertThat(cell, not(dummyObject));
+        assertThat(cell.equals(null), is(false));
+        assertThat(cell.equals(dummyObject), is(false));
     }
 
     @Test
