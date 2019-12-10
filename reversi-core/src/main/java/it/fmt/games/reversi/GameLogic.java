e0d70378a9aa88de72e31edb8031fcae7711c0a4
Matteo <teo_exc@hotmail.it>
2019-12-10 11:46:09 +0100

    reversi-#22 Entity: GameLogic

--- reversi-core/src/main/java/it/fmt/games/reversi/GameLogic.java
+++ reversi-core/src/main/java/it/fmt/games/reversi/GameLogic.java
@@ -1,11 +1,13 @@
 package it.fmt.games.reversi;
 
 public class GameLogic {
+    private Board board;
+    private Player currentPlayer;
+    private Player otherPlayer;
+
     public Board getBoard() {
         return board;
     }
-
-    private Board board;
 
     public GameLogic(Board board) {
         this.board = board;
@@ -15,12 +17,24 @@ public class GameLogic {
         this.board = new Board();
     }
 
-    public void initBoard() {
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
 
+    public Player getOtherPlayer() {
+        return otherPlayer;
+    }
+
+    public void initializeBoard() {
         board.setCell(Coordinates.of("e4"), Piece.PLAYER_1);
         board.setCell(Coordinates.of("d4"), Piece.PLAYER_2);
         board.setCell(Coordinates.of("d5"), Piece.PLAYER_1);
         board.setCell(Coordinates.of("e5"), Piece.PLAYER_2);
-
+        currentPlayer = new Player1();
+        otherPlayer = new Player2();
     }
+
+
+
+
 }
--- reversi-core/src/test/java/it/fmt/games/reversi/GameLogicTest.java
+++ reversi-core/src/test/java/it/fmt/games/reversi/GameLogicTest.java
@@ -3,17 +3,18 @@ package it.fmt.games.reversi;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class GameLogicTest {
 
     @Test
     void initBoard() throws Exception {
         GameLogic gameLogic=new GameLogic();
-        gameLogic.initBoard();
+        gameLogic.initializeBoard();
         Board aspectedBoard= BoardReader.read("gameLogicTest");
 
         assertThat(gameLogic.getBoard(), is(aspectedBoard));
+        assertThat(gameLogic.getCurrentPlayer().getPiece(),is(Piece.PLAYER_1));
+        assertThat(gameLogic.getOtherPlayer().getPiece(),is(Piece.PLAYER_2));
     }
+
 }
