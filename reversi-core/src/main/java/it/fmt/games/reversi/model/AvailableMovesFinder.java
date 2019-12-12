85a9be0e3e631fcf2c14504aa31c220e5d60fb0a
xcesco <abubusoft@gmail.com>
2019-12-12 03:16:58 +0100

    refactoring

--- reversi-core/src/main/java/it/fmt/games/reversi/model/AvailableMovesFinder.java
+++ reversi-core/src/main/java/it/fmt/games/reversi/model/AvailableMovesFinder.java
@@ -1,28 +1,18 @@
 package it.fmt.games.reversi.model;
 
-import it.fmt.games.reversi.exceptions.InvalidPieceSelectedException;
-
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class AvailableMovesFinder {
-    protected final Piece currentPlayer;
-    protected final Piece otherPlayer;
-    protected final Board board;
-
+public class AvailableMovesFinder extends AbstractBoardOperation {
 
     public AvailableMovesFinder(Board board, Piece piece) {
-        this.currentPlayer = piece;
-        this.otherPlayer = piece == Piece.PLAYER_1 ? Piece.PLAYER_2 : Piece.PLAYER_1;
-        this.board = board;
-        if (piece == Piece.EMPTY) throw (new InvalidPieceSelectedException());
+        super(board, piece);
     }
 
     public static List<Coordinates> findMoves(Board board, Piece piece) {
         AvailableMovesFinder finder = new AvailableMovesFinder(board, piece);
-
         return finder.findMoves();
     }
 
@@ -40,20 +30,8 @@ public class AvailableMovesFinder {
 
     private boolean onlyValidCell(Cell cell) {
         return board.isCellContentEqualsTo(cell.getCoordinates(), Piece.EMPTY) &&
-                Stream.of(Direction.values()).anyMatch(direction -> hasPiecesToInvertAlongDirection(cell.getCoordinates(), direction));
+                Stream.of(Direction.values()).anyMatch(direction -> isAnyPieceToInvert(cell.getCoordinates(), direction));
     }
 
-    private boolean hasPiecesToInvertAlongDirection(Coordinates initialCoordinates, Direction direction) {
-        int piecesToReverte = (int) findEnemyPiecesAlongDirection(initialCoordinates, direction).count();
-        return piecesToReverte > 0 &&
-                board.isCellContentEqualsTo(initialCoordinates.translate(direction, piecesToReverte + 1), this.currentPlayer);
-    }
-
-    private Stream<Coordinates> findEnemyPiecesAlongDirection(Coordinates coordinates, Direction direction) {
-
-        return Stream.iterate(coordinates.translate(direction),
-                coords -> board.isCellContentEqualsTo(coords, this.otherPlayer),
-                coords -> coords.translate(direction));
-    }
 
 }
--- reversi-core/src/main/java/it/fmt/games/reversi/model/EnemyPiecesHunter.java
+++ reversi-core/src/main/java/it/fmt/games/reversi/model/EnemyPiecesHunter.java
@@ -1,23 +1,15 @@
 package it.fmt.games.reversi.model;
 
-import it.fmt.games.reversi.exceptions.InvalidPieceSelectedException;
-
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class EnemyPiecesHunter {
+public class EnemyPiecesHunter extends AbstractBoardOperation {
     private final Coordinates searchOrigin;
-    private final Piece piece;
-    private final Board board;
-    private final Piece enemyPiece;
 
     public EnemyPiecesHunter(Board board, Coordinates coords, Piece piece) {
-        if (piece == Piece.EMPTY || piece == null) throw (new InvalidPieceSelectedException());
-        this.piece = piece;
-        this.board = board;
+        super(board, piece);
         this.searchOrigin = coords;
-        this.enemyPiece = piece == Piece.PLAYER_1 ? Piece.PLAYER_2 : Piece.PLAYER_1;
     }
 
     public static List<Coordinates> find(Board board, Coordinates newMoveCoords, Piece piece) {
@@ -37,17 +29,5 @@ public class EnemyPiecesHunter {
 
     private Stream<Coordinates> rotateEnemyPieces(Direction direction) {
         return findEnemyPiecesAlongDirection(searchOrigin, direction);
-    }
-
-    private boolean isAnyPieceToInvert(Coordinates initialCoordinates, Direction direction) {
-        int piecesToReverte = (int) findEnemyPiecesAlongDirection(initialCoordinates, direction).count();
-        return piecesToReverte > 0 &&
-                board.isCellContentEqualsTo(initialCoordinates.translate(direction, piecesToReverte + 1), piece);
-    }
-
-    private Stream<Coordinates> findEnemyPiecesAlongDirection(Coordinates coordinates, Direction direction) {
-        return Stream.iterate(coordinates.translate(direction),
-                coords -> board.isCellContentEqualsTo(coords, enemyPiece),
-                coords -> coords.translate(direction));
     }
 }
