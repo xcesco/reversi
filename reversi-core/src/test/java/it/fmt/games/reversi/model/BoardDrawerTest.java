bda5e518c6206e00ccc1f40e6817176d275a4466
taz00 <tbonifazi91@gmail.com>
2019-12-12 12:03:22 +0100

    fix #28 reversi-#28 Entity: DrawBoard (turn class and method in public for Travis build)

--- reversi-core/src/test/java/it/fmt/games/reversi/model/BoardDrawerTest.java
+++ reversi-core/src/test/java/it/fmt/games/reversi/model/BoardDrawerTest.java
@@ -8,5 +8,7 @@ class BoardDrawerTest {
     void drawBoardTest() throws Exception {
         BoardDrawer boardDrawer = new BoardDrawer(BoardReader.read("available_moves01"));
         boardDrawer.drawBoard();
+        BoardDrawer boardDrawer2 = new BoardDrawer(BoardReader.read("available_moves02"));
+        boardDrawer2.drawBoard();
     }
 }
