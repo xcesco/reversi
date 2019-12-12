504238600ab07522a17560bc8a2831ac5fece484
taz00 <tbonifazi91@gmail.com>
2019-12-12 12:42:15 +0100

    reversi-18 Improve code coverage (covered test on methods equals for class Cell and Coordinates)

--- reversi-core/src/test/java/it/fmt/games/reversi/model/BoardDrawerTest.java
+++ reversi-core/src/test/java/it/fmt/games/reversi/model/BoardDrawerTest.java
@@ -2,10 +2,10 @@ package it.fmt.games.reversi.model;
 
 import org.junit.jupiter.api.Test;
 
-class BoardDrawerTest {
+public class BoardDrawerTest {
 
     @Test
-    void drawBoardTest() throws Exception {
+    public void drawBoardTest() throws Exception {
         BoardDrawer boardDrawer = new BoardDrawer(BoardReader.read("available_moves01"));
         boardDrawer.drawBoard();
     }
