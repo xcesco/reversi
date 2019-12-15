d705c5bb78636080df7314d19d4b50e46452adfd
xcesco <abubusoft@gmail.com>
2019-12-15 16:56:04 +0100

    fix #34
    reversi-#34 DecisionHandlerType: simplification

--- reversi-core/src/main/java/it/fmt/games/reversi/DecisionHandlerType.java
+++ reversi-core/src/main/java/it/fmt/games/reversi/DecisionHandlerType.java
@@ -1,28 +1,20 @@
 package it.fmt.games.reversi;
 
-import it.fmt.games.reversi.exceptions.InvalidDecisionHandlerException;
 import it.fmt.games.reversi.model.RandomDecisionHandler;
 import it.fmt.games.reversi.model.SimpleDecisionHandler;
 
-import java.lang.reflect.InvocationTargetException;
-
 public enum DecisionHandlerType {
-    SIMPLE(SimpleDecisionHandler.class),
-    RANDOM(RandomDecisionHandler.class);
+    SIMPLE(new SimpleDecisionHandler()),
+    RANDOM(new RandomDecisionHandler());
 
-    private final Class<? extends DecisionHandler> clazz;
+    private final DecisionHandler decisionHandler;
 
-    DecisionHandlerType(Class<? extends DecisionHandler> clazz) {
-        this.clazz = clazz;
+    DecisionHandlerType(DecisionHandler decisionHandler) {
+        this.decisionHandler = decisionHandler;
     }
 
     public DecisionHandler createHandler() {
-        try {
-            return clazz.getConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
-            throw new InvalidDecisionHandlerException(e.getMessage());
-        }
+        return decisionHandler;
     }
 
 }
