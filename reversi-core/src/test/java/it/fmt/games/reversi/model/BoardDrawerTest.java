8558849c8cca4fac7fbf9697d9b07ffc9427dab8
taz00 <tbonifazi91@gmail.com>
2019-12-15 14:00:26 +0100

    fix #29 reversi-#29 Make this final field static too

--- reversi-core/src/test/java/it/fmt/games/reversi/model/BoardDrawerTest.java
+++ reversi-core/src/test/java/it/fmt/games/reversi/model/BoardDrawerTest.java
@@ -2,14 +2,17 @@ package it.fmt.games.reversi.model;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class BoardDrawerTest {
 
-//    @Test
-//    public void drawBoardTest() throws Exception {
-//        Board board = BoardReader.read("available_moves01");
-//        BoardDrawer boardDrawer = new BoardDrawer(board);
-//        boardDrawer.drawBoard();
-//    }
+    @Test
+    public void drawBoardTest() throws Exception {
+        Board board = BoardReader.read("available_moves01");
+        BoardDrawer boardDrawer = new BoardDrawer(board);
+        boardDrawer.drawBoard();
+        assertThat(true, is(true));
+    }
 }
