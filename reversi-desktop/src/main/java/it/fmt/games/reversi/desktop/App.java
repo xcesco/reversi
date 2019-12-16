c77bacf09695148495a499ba808c424f8bc2f424
Matteo <teo_exc@hotmail.it>
2019-12-16 14:06:41 +0100

    reversi-#33 Project Desktop

--- reversi-desktop/src/main/java/it/fmt/games/reversi/desktop/App.java
+++ reversi-desktop/src/main/java/it/fmt/games/reversi/desktop/App.java
@@ -13,13 +13,16 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class App extends Canvas implements MouseListener, GameRenderer {
-    public static final int CELL_SIZE = 70;
-    public static final int BASE_X = 35;
-    public static final int BASE_Y = 105;
-    public static final int OFFSET = 25;
+    public static double RESIZE = 1.3;
+    public static final int CELL_SIZE = (int) (70 / RESIZE);
+    public static final int BASE_X = (int) (35 / RESIZE);
+    public static final int BASE_Y = (int) (105 / RESIZE);
+    public static final int OFFSET = (int) (25 / RESIZE);
     private static final boolean SHOW_LABELS = true;
     public static final Color darkGreen = new Color(0, 120, 0);
-    public static final Color brown = new Color(153,102, 0);
+    public static final Color brown = new Color(153, 102, 0);
+    public static final int WIDTH = (int) (650 / RESIZE);
+    public static final int HEIGHT = (int) (900 / RESIZE);
     public static String winner = "";
     public GameSnapshot gameSnapshot;
     private boolean started = false;
@@ -32,7 +35,7 @@ public class App extends Canvas implements MouseListener, GameRenderer {
 
     public static void main(String[] args) {
         JFrame win = new JFrame("Reversi");
-        win.setSize(650, 900);
+        win.setSize(WIDTH, HEIGHT);
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         win.setResizable(false);
         win.add(new App());
@@ -44,30 +47,31 @@ public class App extends Canvas implements MouseListener, GameRenderer {
         boxes = new Rectangle[8][8];
         IntStream.range(0, Board.BOARD_SIZE)
                 .forEach(row -> IntStream.range(0, Board.BOARD_SIZE)
-                        .forEach(col -> boxes[row][col] = new Rectangle(BASE_X + col * 70, BASE_Y + row * 70, 70, 70)));
+                        .forEach(col -> boxes[row][col] = new Rectangle(BASE_X + col * CELL_SIZE, BASE_Y + row * CELL_SIZE, CELL_SIZE, CELL_SIZE)));
     }
 
     public void paint(Graphics g) {
         g.setColor(Color.lightGray);
-        g.fillRect(0, 0, 800, 900);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
         g.setColor(Color.black);
         Graphics2D g2 = (Graphics2D) g;
-        g.setFont(new Font("Arial", Font.BOLD, 48));
+        g.setFont(new Font("Arial", Font.BOLD, (int) (48 / RESIZE)));
 
         if (!started) {
-            g.drawString("REVERSI", 250- OFFSET, 300);
-            g.drawString("Click to Play", 215- OFFSET, 400);
+            g.drawString("REVERSI", (int) (225 / RESIZE), (int) (300 / RESIZE));
+            g.drawString("Click to Play", (int) (190 / RESIZE), (int) (400 / RESIZE));
         } else if (this.gameSnapshot != null) {
             // board
-
             g.setColor(brown);
-            g.fillRect(BASE_X-10,BASE_Y-10,580,580);
-
+            g.fillRect(BASE_X - (int)(10/RESIZE),
+                    BASE_Y - (int)(10/RESIZE),
+                    CELL_SIZE * 8 + (int) (20 / RESIZE),
+                    CELL_SIZE * 8 + (int) (20 / RESIZE));
             g.setColor(darkGreen);
-            g.fillRect(BASE_X,BASE_Y,560,560);
+            g.fillRect(BASE_X, BASE_Y, CELL_SIZE * 8, CELL_SIZE * 8);
             g.setColor(Color.BLACK);
-            g.setFont(new Font("Arial", Font.BOLD, 48));
-            g.drawString("REVERSI", 240, 50);
+            g.setFont(new Font("Arial", Font.BOLD, (int) (48 / RESIZE)));
+            g.drawString("REVERSI", (int) (240 / RESIZE), (int) (50 / RESIZE));
             IntStream.range(0, Board.BOARD_SIZE)
                     .forEach(row -> IntStream.range(0, Board.BOARD_SIZE)
                             .forEach(col -> g2.draw(boxes[row][col])));
@@ -79,36 +83,57 @@ public class App extends Canvas implements MouseListener, GameRenderer {
                     } else {
                         g.setColor(Color.black);
                     }
-                    g.fillOval(BASE_X + item.getCoordinates().getRow() * CELL_SIZE + 5, BASE_Y + item.getCoordinates().getColumn() * CELL_SIZE + 5, CELL_SIZE - 10, CELL_SIZE - 10);
+                    g.fillOval(BASE_X + item.getCoordinates().getRow() * CELL_SIZE + (int) (5 / RESIZE),
+                            BASE_Y + item.getCoordinates().getColumn() * CELL_SIZE + (int) (5 / RESIZE),
+                            CELL_SIZE - (int) (10 / RESIZE),
+                            CELL_SIZE - (int) (10 / RESIZE));
                 }
 
             });
             //Legal Moves
             this.gameSnapshot.getAvailableMoves().getMovesActivePlayer().forEach(item -> {
                 g.setColor(Color.gray);
-                g.fillOval(BASE_X + item.getRow() * CELL_SIZE + 30, BASE_Y + item.getColumn() * CELL_SIZE + 30, CELL_SIZE - 60, CELL_SIZE - 60);
+                g.fillOval(BASE_X + item.getRow() * CELL_SIZE + (int) (30 / RESIZE),
+                        BASE_Y + item.getColumn() * CELL_SIZE + (int) (30 / RESIZE),
+                        CELL_SIZE - (int) (60 / RESIZE),
+                        CELL_SIZE - (int) (60 / RESIZE));
             });
 
             //Score
             g.setColor(Color.BLACK);
-            g.setFont(new Font("Arial", Font.BOLD, 36));
-            g.drawString(" : " + gameSnapshot.getScore().getPlayer2Score(), 100, 740+OFFSET);
-            g.drawString(" : " + gameSnapshot.getScore().getPlayer1Score(), 520, 740+OFFSET);
+            g.setFont(new Font("Arial", Font.BOLD, (int) (36 / RESIZE)));
+            g.drawString(" : " + gameSnapshot.getScore().getPlayer2Score(),
+                    (int) (100 / RESIZE), (int) (765 / RESIZE));
+            g.drawString(" : " + gameSnapshot.getScore().getPlayer1Score(),
+                    (int) (520 / RESIZE), (int) (765 / RESIZE));
 
 
-            g.fillOval(35, 700+OFFSET, CELL_SIZE - 10, CELL_SIZE - 10);
+            g.fillOval((int) (35 / RESIZE), (int) (725 / RESIZE),
+                    CELL_SIZE - (int) (10 / RESIZE),
+                    CELL_SIZE - (int) (10 / RESIZE));
             g.setColor(Color.white);
-            g.fillOval(455, 700+OFFSET, CELL_SIZE - 10, CELL_SIZE - 10);
+            g.fillOval((int) (455 / RESIZE), (int) (725 / RESIZE),
+                    CELL_SIZE - (int) (10 / RESIZE),
+                    CELL_SIZE - (int) (10 / RESIZE));
 
             // labels
             if (SHOW_LABELS) {
-                String[] C = {"A", "B", "C", "D", "E", "F", "G", "H"};
-                g.setFont(new Font("Arial", Font.PLAIN, 24));
+                String[] Label = {"A", "B", "C", "D", "E", "F", "G", "H"};
+                g.setFont(new Font("Arial", Font.PLAIN, (int) (24 / RESIZE)));
                 g.setColor(Color.black);
-                IntStream.range(0, Board.BOARD_SIZE).forEach(col -> g.drawString(C[col], BASE_X + col * 70 + 30, BASE_Y - 15));
-                IntStream.range(0, Board.BOARD_SIZE).forEach((row -> g.drawString(String.valueOf(row + 1), BASE_X - 25,  BASE_Y + row * 70+ 45)));
-                IntStream.range(0, Board.BOARD_SIZE).forEach(col -> g.drawString(C[col], BASE_X + col * 70 + 30, BASE_Y+610 - 15));
-                IntStream.range(0, Board.BOARD_SIZE).forEach((row -> g.drawString(String.valueOf(row + 1), BASE_X + 600 - 25,  BASE_Y + row * 70+ 45)));
+                IntStream.range(0, Board.BOARD_SIZE).forEach(col ->
+                        g.drawString(Label[col], BASE_X + col * CELL_SIZE + (int) (30 / RESIZE),
+                                BASE_Y - (int) (15 / RESIZE)));
+                IntStream.range(0, Board.BOARD_SIZE).forEach((row -> g.drawString(String.valueOf(row + 1),
+                        BASE_X - (int) (25 / RESIZE),
+                        BASE_Y + row * CELL_SIZE + (int) (45 / RESIZE))));
+
+                IntStream.range(0, Board.BOARD_SIZE).forEach(col -> g.drawString(Label[col],
+                        BASE_X + col * CELL_SIZE + (int) (30 / RESIZE),
+                        BASE_Y + CELL_SIZE*8 + (int) (30 / RESIZE)));
+                IntStream.range(0, Board.BOARD_SIZE).forEach((row -> g.drawString(String.valueOf(row + 1),
+                        BASE_X + CELL_SIZE*8 + (int) (10 / RESIZE),
+                        BASE_Y + row * CELL_SIZE + (int) (45 / RESIZE))));
 
             }
 
@@ -131,21 +156,27 @@ public class App extends Canvas implements MouseListener, GameRenderer {
                     e.printStackTrace();
                 }
                 g.setColor(Color.lightGray);
-                g.fillRect(0, 0, 800, 900);
+                g.fillRect(0, 0, WIDTH, HEIGHT);
                 g.setColor(Color.black);
-                g.setFont(new Font("Arial", Font.BOLD, 48));
-                g.drawString(winner, 150-OFFSET, 300);
+                g.setFont(new Font("Arial", Font.BOLD, (int) (48 / RESIZE)));
+                g.drawString(winner, (int) (125 / RESIZE), (int) (300 / RESIZE));
 
                 g.setColor(Color.BLACK);
-                g.fillOval(145, 360, CELL_SIZE - 10, CELL_SIZE - 10);
+                g.fillOval((int) (145 / RESIZE), (int) (360 / RESIZE),
+                        CELL_SIZE - (int) (10 / RESIZE),
+                        CELL_SIZE - (int) (10 / RESIZE));
 
                 g.setColor(Color.white);
-                g.fillOval(400, 360, CELL_SIZE - 10, CELL_SIZE - 10);
+                g.fillOval((int) (400 / RESIZE), (int) (360 / RESIZE),
+                        CELL_SIZE - (int) (10 / RESIZE),
+                        CELL_SIZE - (int) (10 / RESIZE));
 
                 g.setColor(Color.gray);
-                g.setFont(new Font("Arial", Font.BOLD, 36));
-                g.drawString("" + gameSnapshot.getScore().getPlayer2Score(), 155, 400);
-                g.drawString("" + gameSnapshot.getScore().getPlayer1Score(), 410, 400);
+                g.setFont(new Font("Arial", Font.BOLD, (int) (36 / RESIZE)));
+                g.drawString("" + gameSnapshot.getScore().getPlayer2Score(),
+                        (int) (155 / RESIZE), (int) (400 / RESIZE));
+                g.drawString("" + gameSnapshot.getScore().getPlayer1Score(),
+                        (int) (410 / RESIZE), (int) (400 / RESIZE));
             }
 
         }
@@ -158,8 +189,8 @@ public class App extends Canvas implements MouseListener, GameRenderer {
 
             //reversi = new Reversi(this, this, PlayerFactory.createRoboPlayer1(new DesktopDecisionHandler()), PlayerFactory.createRoboPlayer2(new DesktopDecisionHandler()));
 
-            gameLogic = new GameLogicThread(this, PlayerFactory.createUserPlayer1(), PlayerFactory.createUserPlayer2(), this);
-            //gameLogic = new GameLogicThread(this, PlayerFactory.createUserPlayer1(), PlayerFactory.createRoboPlayer2(), this);
+            //gameLogic = new GameLogicThread(this, PlayerFactory.createUserPlayer1(), PlayerFactory.createUserPlayer2(), this);
+            gameLogic = new GameLogicThread(this, PlayerFactory.createUserPlayer1(), PlayerFactory.createRoboPlayer2(), this);
             //gameLogic = new GameLogicThread(this, PlayerFactory.createRoboPlayer1(), PlayerFactory.createRoboPlayer2(), this);
             gameLogic.start();
             started = true;
