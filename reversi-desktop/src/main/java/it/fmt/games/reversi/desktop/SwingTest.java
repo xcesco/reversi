090d566ff3d747a9c97c26220171831718bd6324
Matteo <teo_exc@hotmail.it>
2019-12-19 16:28:32 +0100

    reversi-#33 Project Desktop

--- reversi-desktop/src/main/java/it/fmt/games/reversi/desktop/SwingTest.java
+++ reversi-desktop/src/main/java/it/fmt/games/reversi/desktop/SwingTest.java
@@ -3,6 +3,7 @@ package it.fmt.games.reversi.desktop;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
@@ -30,14 +31,12 @@ public class SwingTest extends JFrame {
 
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         String path = Objects.requireNonNull(classLoader.getResource("logo.png")).getPath();
-        var image = ImageIO.read(new File(path));
+        BufferedImage image = ImageIO.read(new File(path));
         setIconImage(image);
-
+        BufferedImage resized = resize(image, 250, 250);
 
         setTitle("FMT-Reversi");
-        setSize(600, 800);
-
-
+        setSize(300, 500);
 
         Container container = getContentPane();
         container.setLayout(new GridBagLayout());
@@ -50,7 +49,7 @@ public class SwingTest extends JFrame {
         gbc.anchor = GridBagConstraints.PAGE_START;
         container.add(title,gbc);
 
-        JLabel picLabel = new JLabel(new ImageIcon(image));
+        JLabel picLabel = new JLabel(new ImageIcon(resized));
         gbc.gridx=0;
         gbc.gridy=1;
         gbc.weightx=1;
@@ -60,8 +59,8 @@ public class SwingTest extends JFrame {
         JLabel mode = new JLabel("Please select mode: ");
         gbc.gridx=0;
         gbc.gridy=2;
-//        gbc.weightx=1;
-//        gbc.weighty=1;
+        gbc.weightx=1;
+        gbc.weighty=1;
         container.add(mode,gbc);
 
         JButton player_vs_player = new JButton("Player VS Player");
@@ -102,27 +101,6 @@ public class SwingTest extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-
-        /*Container container = getContentPane();
-        container.setLayout(new FlowLayout());
-
-        JPanel r = new JPanel(new BorderLayout());
-        try {
-            r.add(new JLabel(new ImageIcon(ImageIO.read(new File("./temp.png")))));
-        }catch (IOException e) {
-            e.getMessage().toString();
-        }
-        Jbutton j = new JButton("Recycle Item");
-        r.add(j, BorderLayout.SOUTH);
-        container.add(r);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setSize(500,500);
-        setVisible(true);*/
-
-
-
         player_vs_player.addActionListener(actionEvent -> initGame(P1_VS_P2));
         player_vs_cpu.addActionListener(actionEvent -> initGame(P1_VS_CPU));
         cpu_vs_player.addActionListener(actionEvent -> initGame(CPU_VS_P2));
@@ -140,5 +118,13 @@ private void initGame(int gameType){
     frame.setVisible(true);
 
 }
+    private static BufferedImage resize(BufferedImage img, int height, int width) {
+        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resized;
+    }
 
 }
